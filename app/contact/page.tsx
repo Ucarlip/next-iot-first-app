079d98d69f510a4b909904523d34462129ae456b
Ucarlip <modkak98@gmail.com>
2026-01-13 10:30:01 +0700

    commit No.3 : เเก้ไข app/page เพิ่ม page About Contact and Faculty

---
+++ app/contact/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Contact() {
+  return (
+    <>
+      <h1 className="text-center font-bold mt-10">Contact Page</h1>
+      <hr />
+      <p className="text-center">
+        This is the contact page. Here you can find contact information for this website.
+      </p>
+    </>
+  )
+}
---
+++ app/faculty/engineer/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function faculty() {
+  return (
+    <>
+      <h1 className="text-center font-bold mt-10">Faculty of Engineering</h1>
+      <hr />
+      <p className="text-center">
+        This is the faculty page for the Faculty of Engineering.
+      </p>
+    </>
+  )
+}
